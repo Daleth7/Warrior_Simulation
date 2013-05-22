6b01c45b823ad4152e8fa49ab015ed9ba51a79d5
Giblit <ajgibson@hotmail.com>
2013-05-21 19:25:20 -0600

    Update (WIP) Environment.hpp
    Updated the header to make it easier to understand and easier to update. Probably could do something better than friending the main class but I couldn't think of anything I was going to use derived classes and protected variables but couldn't figure out how to change a protected variable in the main class from the derived ones( can do the other way though .)

--- (WIP) Environment.hpp
+++ (WIP) Environment.hpp
@@ -2,130 +2,54 @@
 #define ENVIRONMENT_H
 #include <string>   //std::string
 #include <iostream> //std::ostream
+#include "warrior.h" /**So I can later make certian warriors*
+                       *better with different environments. **/
+
+enum condition { Desert , Woodland , Grassland , Highland , Artic , Tundra ,
+ Rainforest , Raining = 0 , Windy , Snowing , Hailing , Normal };
+
+namespace NAMES
+{
+    const std::string ecosytem[ 7 ] = { "Desert" , "Woodland" , "Grassland" ,
+     "Highland" , "Artic" , "Tundra" , "Rainforest" } ,
+    weather[ 5 ] = { "Raining" , "Windy" , "Snowing" , "Hailing" , "Normal" };
+}
 
 class Ecosystem;
 class Weather;
 
 class Environment
 {
+    friend std::ostream& operator<< ( std::ostream& , const Environment& );
     public:
         Environment( void );
         Environment( const Ecosystem& , const Weather& );
-        Environment( const Ecosystem& , const std::string& );
-        Environment( const Ecosystem& , const unsigned short& );
-        Environment( const std::string& , const Weather& );
-        Environment( const std::string& , const std::string& );
-        Environment( const std::string& , const unsigned& );
-        Environment( const unsigned short& , const Weather& );
-        Environment( const unsigned short& , const std::string& );
-        Environment( const unsigned short& , const unsigned short& );
-
-        friend std::ostream& operator<< ( std::ostream& , const Environment& );
-
-    protected:
-        unsigned short disadvantage;
+        Environment( const condition& , const condition& );
+    private:
+        //unsigned short disadvantage;
         const std::string ecosytemName , weatherName;
 };
 
-class Desert;
-class Woodland;
-class Grassland;
-class Highland;
-class Artic;
-class Tundra;
-class Rainforest;
-
-class Ecosystem : public Environment
+class Ecosystem
 {
+    friend Environment;
+    friend std::ostream& operator<< ( std::ostream& , const Ecosystem& );
 public:
     Ecosystem( void );
-    Ecosystem( const std::string& );
-    Ecosystem( const unsigned short& );
-    Ecosystem( const Desert& );
-    Ecosystem( const Woodland& );
-    Ecosystem( const Grassland& );
-    Ecosystem( const Highland& );
-    Ecosystem( const Artic& );
-    Ecosystem( const Tundra& );
-    Ecosystem( const Rainforest& );
+    Ecosystem( const condition& );
+private:
+    const std::string name;
 };
 
-class Desert : public Ecosystem
+class Weather
 {
-public:
-    Desert( void );
-};
-
-class Woodland : public Ecosystem
-{
-public:
-    Woodland( void );
-};
-
-class Grassland : public Ecosystem
-{
-public:
-    Grassland( void );
-};
-
-class Highland : public Ecosystem
-{
-public:
-    Highland( void );
-};
-
-class Artic : public Ecosystem
-{
-public:
-    Artic( void );
-};
-
-class Tundra : public Ecosystem
-{
-public:
-    Tundra( void );
-};
-
-class Rainforest : public Ecosystem
-{
-public:
-    Rainforest( void );
-};
-
-class Raining;
-class Windy;
-class Snowing;
-class Hailing;
-
-class Weather : public Environment
-{
+    friend Environment;
+    friend std::ostream& operator<< ( std::ostream& , const Weather& );
 public:
     Weather( void );
-    Weather( const std::string& );
-    Weather( const unsigned short& );
-    Weather( const Raining& );
-    Weather( const Windy& );
-    Weather( const Snowing& );
-    Weather( const Hailing& );
+    Weather( const condition& );
+private:
+    const std::string name;
 };
 
-class Raining : public Weather
-{
-    Raining( void );
-};
-
-class Windy : public Weather
-{
-    Windy( void );
-};
-
-class Snowing : public Weather
-{
-    Snowing( void );
-};
-
-class Hailing : public Weather
-{
-    Hailing( void );
-};
 #endif // ENVIRONMENT_H
