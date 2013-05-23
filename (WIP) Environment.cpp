42a6b8967f7bb9fb8c9003d83b7658a3221f1141
Giblit <ajgibson@hotmail.com>
2013-05-22 18:21:02 -0600

    Update (WIP) Environment.cpp

--- (WIP) Environment.cpp
+++ (WIP) Environment.cpp
@@ -9,7 +9,69 @@ min_rock_height( 0 ) , max_rock_height( 0 ) {}
 
 Terrain::Terrain( const default_terrain &ter ) : Terrain()
 {
-    //going to put all the defaults here
+    switch( ter )
+    {
+    case Forest:
+        terrain_name = "Forest";
+        trees = 50;
+        rocks = 10;
+        min_tree_width = 1;
+        max_tree_width = 3;
+        min_tree_height = 1;
+        max_tree_height = 100;
+        min_rock_width = 1;
+        max_rock_width = 2;
+        min_rock_height = 1;
+        max_rock_height = 5;
+        break;
+    case Jungle:
+        terrain_name = "Jungle";
+        trees = 200;
+        min_tree_width = 1;
+        max_tree_width = 5;
+        min_tree_height =1;
+        max_tree_height = 100;
+        break;
+    case Plain:
+        terrain_name = "Plain";
+        break;
+    case Mountain:
+        terrain_name = "Mountain";
+        elevation = 1000;
+        trees = 10;
+        rocks = 50;
+        min_tree_width = 1;
+        max_tree_width = 3;
+        min_tree_height = 1;
+        max_tree_height = 100;
+        min_rock_width = 1;
+        max_rock_width = 10;
+        min_rock_height = 1;
+        max_rock_height = 10;
+        break;
+    case Hill:
+        terrain_name = "Hill";
+        elevation = 200;
+        trees = 2;
+        rocks = 10;
+        min_tree_width = 1;
+        max_tree_width = 3;
+        min_tree_height = 1;
+        max_tree_height = 100;
+        min_rock_width = 1;
+        max_rock_width = 3;
+        min_rock_height = 1;
+        max_rock_height = 3;
+        break;
+    case Rocky:
+        terrain_name = "Rocky";
+        rocks = 50;
+        min_rock_width = 1;
+        max_rock_width = 10;
+        min_rock_height = 1;
+        max_rock_height = 10;
+        break;
+    }
 }
 
 const unsigned short Terrain::generate_random_number( const unsigned short &min , const unsigned short &max ) const
